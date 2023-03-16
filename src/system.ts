ed8ed0b40f5049c133cac5107a2b75206dd6ca1e
zsh <642696259@qq.com>
2023-03-16 22:14:45 +0800

    first commit

---
+++ src/system.ts
@@ -0,0 +1,19 @@
+export const setting = {
+  continuousDialogue: true,
+  archiveSession: false,
+  openaiAPIKey: "",
+  openaiAPITemperature: 60,
+  password: "",
+  systemRule: ""
+}
+
+export const message = `- 感谢关注智能边界公众号  ,BianJie.AI为团队作为目前一个研究与探索的雏形，现在免费向用户使用，请尽量期待团队产品。
+- 自我介绍：我是一个功能强大的AI工具，可以帮助你更轻松地完成各种任务和活动。无论你是在学习、工作、生活中，我都是你的忠实助手，带给你更多便捷与享受。
+
+
+
+`
+
+export type Setting = typeof setting
+
+export const resetContinuousDialogue = false
